499d77c01a51bf72530fa925ea2dd70629370c82
Intira-72 <intira.vinit@gmail.com>
2023-03-02 13:39:49 +0700

    Django Rest Framework CreateAPIView

--- django-rest-api-project/backend/products/urls.py
+++ django-rest-api-project/backend/products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='product'),
+    path('', views.ProductCreateAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
 ]
 
