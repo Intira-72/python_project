50b251e009b27ce8798f67b3781d2c007366c1ca
Intira-72 <intira.vinit@gmail.com>
2023-03-03 16:04:53 +0700

    Django Templates Basics

--- django-32-project/asticles/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AsticlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'asticles'
--- django-rest-api-project/frontend/product_detail.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-endpoint = "http://localhost:8000/api/product/"
-
-get_response = requests.get(endpoint)
-
-
-if __name__=="__main__":
-    print(get_response.json())
