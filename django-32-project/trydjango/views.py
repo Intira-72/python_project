7047634efe3c49a59c893afa0390b5b2ac77c2e2
Intira-72 <intira.vinit@gmail.com>
2023-03-07 14:04:42 +0700

    Register Model in the Admin

--- django-32-project/trydjango/views.py
+++ django-32-project/trydjango/views.py
@@ -2,12 +2,15 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string, get_template
 
 from asticles.models import Article
+import random
 
 
 def home(request, *args, **kwargs):
+    article_obj = Article.objects.get(pk=random.randint(1, 3))
     article_queryset = Article.objects.all()
 
-    context = {'article_queryset': article_queryset}
+    context = {'article_queryset': article_queryset,
+               'article_obj': article_obj}
 
     tmpl = get_template("home_view.html")
     html_str = tmpl.render(context=context)
