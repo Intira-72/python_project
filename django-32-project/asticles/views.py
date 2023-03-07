e5c95665841417403492213577db0ffc35dafff5
Intira-72 <intira.vinit@gmail.com>
2023-03-07 15:29:28 +0700

    Search Form & Request Data

--- django-32-project/asticles/views.py
+++ django-32-project/asticles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 
-# Create your views here.
+# Article All View
 def article_view(request, pk=None):
     if pk is not None:
         article_obj = Article.objects.get(pk=pk)
@@ -10,3 +10,24 @@ def article_view(request, pk=None):
         context = {'article_obj': None}
 
     return render(request, "article_detail_view.html", context=context)
+
+
+# Article Search View
+def article_search_view(request):
+    
+    try:
+        query = int(request.GET['q'])
+    except:
+        query = None
+
+
+    if query is not None:
+        article_queryset = [Article.objects.get(pk=query)]
+    else:
+        article_queryset = Article.objects.all()
+
+    context = {
+        'article_queryset': article_queryset
+    }
+
+    return render(request, "article_search_view.html", context=context)
--- django-32-project/trydjango/urls.py
+++ django-32-project/trydjango/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views
-from asticles.views import article_view
+from asticles.views import article_view, article_search_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('article/<int:pk>/', article_view),
+    path('articles/', article_search_view),
+    path('article/<int:pk>/', article_view),    
 ]
