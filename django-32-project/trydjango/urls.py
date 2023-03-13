a37ba1fd5defa84f76aaa1eaff2c6200e4f18157
Intira-72 <intira.vinit@gmail.com>
2023-03-13 17:50:17 +0700

    Create a Login View to Authenticate Users

--- django-32-project/trydjango/urls.py
+++ django-32-project/trydjango/urls.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
-from asticles.views import article_view, article_search_view, article_create_form
+from accounts.views import login_view
+from asticles.views import (
+    article_view,
+    article_search_view,
+    article_create_form
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+    path('login', login_view),
     path('articles/', article_search_view),
     path('articles/create/', article_create_form),
-    path('article/<int:pk>/', article_view),    
+    path('articles/<int:pk>/', article_view),    
 ]
